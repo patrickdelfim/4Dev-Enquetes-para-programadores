08bc496d70a140b7fe80ad4201a310dc08981f92
Patrick Delfim <patrickdelfim@gmail.com>
2022-03-03 21:49:10 -0300

    refactor: change Validation to recieve an object instead of value

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,5 +1,6 @@
 import { EmailValidation, MinLengthValidation, RequiredFieldValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
+import { CompareFieldsValidation } from '../compare-fields/compare-fields-validation'
 import faker from '@faker-js/faker'
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
@@ -14,11 +15,18 @@ describe('ValidationBuilder', () => {
     expect(validations).toEqual([new EmailValidation(field)])
   })
 
-  test('should return EmailValidation', () => {
+  test('should return MinLengthValidation', () => {
     const field = faker.database.column()
     const length = faker.datatype.number()
     const validations = sut.field(field).min(length).build()
     expect(validations).toEqual([new MinLengthValidation(field, length)])
+  })
+
+  test('should return CompareFieldsValidation', () => {
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const validations = sut.field(field).sameAs(fieldToCompare).build()
+    expect(validations).toEqual([new CompareFieldsValidation(field, fieldToCompare)])
   })
 
   test('should return a list of validations', () => {
--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -2,18 +2,28 @@ import { InvalidFieldError } from '@/validation/errors'
 import { CompareFieldsValidation } from './compare-fields-validation'
 import faker from '@faker-js/faker'
 
-const makeSut = (valueToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(faker.database.column(), valueToCompare)
+const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(field, fieldToCompare)
 describe('CompareFieldsValidation', () => {
   test('should return error if compare is invalid', () => {
-    const sut = makeSut(faker.random.word())
-    const error = sut.validate(faker.random.word())
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const sut = makeSut(field, fieldToCompare)
+    const error = sut.validate({
+      [field]: faker.random.word(),
+      [fieldToCompare]: faker.random.word()
+    })
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('should return falsy if compare is valid', () => {
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
     const valuetoCompare = faker.random.word()
-    const sut = makeSut(valuetoCompare)
-    const error = sut.validate(valuetoCompare)
+    const sut = makeSut(field, fieldToCompare)
+    const error = sut.validate({
+      [field]: valuetoCompare,
+      [fieldToCompare]: valuetoCompare
+    })
     expect(error).toBeFalsy()
   })
 })

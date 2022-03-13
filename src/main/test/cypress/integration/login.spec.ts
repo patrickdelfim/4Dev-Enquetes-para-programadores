41c4e7b9414356f11ca2df282ebbf86d10bb12dd
Patrick Delfim <patrickdelfim@gmail.com>
2022-03-13 18:41:56 -0300

    refactor: improve input layout

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -6,37 +6,44 @@ describe('Login', () => {
     cy.visit('login')
   })
   it('should load with correct initial state ', () => {
-    cy.getByTestId('email-status')
-      .should('have.attr', 'title', 'Campo obrigatorio')
-      .should('contain.text', '🔴')
-    cy.getByTestId('password-status')
-      .should('have.attr', 'title', 'Campo obrigatorio')
-      .should('contain.text', '🔴')
+    cy.getByTestId('email-wrap').should('have.attr', 'data-status', 'invalid')
+    cy.getByTestId('email').should('have.attr', 'title', 'Campo obrigatorio')
+    cy.getByTestId('email-label').should('have.attr', 'title', 'Campo obrigatorio')
+
+    cy.getByTestId('password-wrap').should('have.attr', 'data-status', 'invalid')
+    cy.getByTestId('password').should('have.attr', 'title', 'Campo obrigatorio')
+    cy.getByTestId('password-label').should('have.attr', 'title', 'Campo obrigatorio')
+
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
   it('should present error state if is invalid', () => {
     cy.getByTestId('email').type(faker.random.word())
-    cy.getByTestId('email-status')
-      .should('have.attr', 'title', 'valor inválido')
-      .should('contain.text', '🔴')
+    cy.getByTestId('email-wrap').should('have.attr', 'data-status', 'invalid')
+    cy.getByTestId('email').should('have.attr', 'title', 'valor inválido')
+    cy.getByTestId('email-label').should('have.attr', 'title', 'valor inválido')
+
     cy.getByTestId('password').type(faker.random.alphaNumeric(3))
-    cy.getByTestId('password-status')
-      .should('have.attr', 'title', 'valor inválido')
-      .should('contain.text', '🔴')
+    cy.getByTestId('password-wrap').should('have.attr', 'data-status', 'invalid')
+    cy.getByTestId('password').should('have.attr', 'title', 'valor inválido')
+    cy.getByTestId('password-label').should('have.attr', 'title', 'valor inválido')
+
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
   it('should present valid state if is valid', () => {
     cy.getByTestId('email').type(faker.internet.email())
-    cy.getByTestId('email-status')
-      .should('have.attr', 'title', 'Tudo certo!')
-      .should('contain.text', '🟢')
+    cy.getByTestId('email-wrap').should('have.attr', 'data-status', 'valid')
+    cy.getByTestId('email').should('not.have.attr', 'title')
+    cy.getByTestId('email-label').should('not.have.attr', 'title')
+
     cy.getByTestId('password').type(faker.internet.password(5))
-    cy.getByTestId('password-status')
-      .should('have.attr', 'title', 'Tudo certo!')
-      .should('contain.text', '🟢')
+
+    cy.getByTestId('password-wrap').should('have.attr', 'data-status', 'valid')
+    cy.getByTestId('password').should('not.have.attr', 'title')
+    cy.getByTestId('password-label').should('not.have.attr', 'title')
+
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })

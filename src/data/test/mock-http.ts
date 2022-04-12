6232822cfc4a2f270448dacc756f7f6078f35c9d
Patrick Delfim <patrickdelfim@gmail.com>
2022-04-11 21:09:41 -0300

    test: ensure AxiosHttpClient calls axios with correct values

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -7,7 +7,8 @@ export const mockPostRequest = (): HttpPostParams => ({
 })
 
 export const mockGetRequest = (): HttpGetParams => ({
-  url: faker.internet.url()
+  url: faker.internet.url(),
+  headers: JSON.parse(faker.datatype.json())
 })
 
 export class HttpPostClientSpy<R> implements HttpPostClient<R> {
--- src/infra/http/axios-http-client/axios-http-client-adapter.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client-adapter.spec.ts
@@ -52,7 +52,7 @@ describe('AxiosHttpClientAdapter', () => {
       const request = mockGetRequest()
       const { sut, mockedAxios } = makeSut()
       await sut.get(request)
-      expect(mockedAxios.get).toHaveBeenCalledWith(request.url)
+      expect(mockedAxios.get).toHaveBeenCalledWith(request.url, { headers: request.headers })
     })
 
     test('should return the correct response on axios.get', async () => {

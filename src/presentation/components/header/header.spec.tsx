3e17b38c811a61a57c8e8453309172b57b97552a
Patrick Delfim <patrickdelfim@gmail.com>
2022-04-16 13:09:36 -0300

    test: ensure header renders username correctly

--- src/presentation/components/header/header.spec.tsx
+++ src/presentation/components/header/header.spec.tsx
@@ -5,17 +5,19 @@ import { createMemoryHistory, MemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 import React from 'react'
 import { AccountModel } from '@/domain/models'
+import { mockAccountModel } from '@/domain/test'
 
 type SutTypes = {
   history: MemoryHistory
   setCurrentAccountMock: (account: AccountModel) => void
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (account = mockAccountModel()): SutTypes => {
   const setCurrentAccountMock = jest.fn()
+
   const history = createMemoryHistory({ initialEntries: ['/'] })
   render(
-    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }} >
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => account }} >
       <Router location={history.location} navigator={history} >
         <Header />
       </Router>
@@ -34,5 +36,11 @@ describe('Header Component', () => {
     fireEvent.click(screen.getByTestId('logout'))
     expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
     expect(history.location.pathname).toBe('/login')
+  })
+
+  test('should render username correctly', () => {
+    const account = mockAccountModel()
+    makeSut(account)
+    expect(screen.getByTestId('username')).toHaveTextContent(account.name)
   })
 })

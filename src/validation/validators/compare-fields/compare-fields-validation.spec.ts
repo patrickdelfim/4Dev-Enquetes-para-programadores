5f20eeb17b3c75ecf21d4fa88b33db9438bb6647
Patrick Delfim <patrickdelfim@gmail.com>
2022-04-21 17:49:37 -0300

    fix: fix unit test issue and try to improve CI/CD

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -5,8 +5,8 @@ import faker from '@faker-js/faker'
 const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(field, fieldToCompare)
 describe('CompareFieldsValidation', () => {
   test('should return error if compare is invalid', () => {
-    const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const field = 'any_field'
+    const fieldToCompare = 'other_field'
     const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({
       [field]: 'any_value',
@@ -16,8 +16,8 @@ describe('CompareFieldsValidation', () => {
   })
 
   test('should return falsy if compare is valid', () => {
-    const field = faker.database.column()
-    const fieldToCompare = faker.database.column()
+    const field = 'any_field'
+    const fieldToCompare = 'other_field'
     const valuetoCompare = faker.random.word()
     const sut = makeSut(field, fieldToCompare)
     const error = sut.validate({

e591fbc180f12ab7e673329217214450d6de1eb2
Patrick Delfim <patrickdelfim@gmail.com>
2022-04-27 14:51:48 -0300

    refactor: move Loading to its own file

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -4,6 +4,7 @@ import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '../adapters/
 import { PrivateRoute } from '@/presentation/components'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
+import { SurveyResult } from '@/presentation/pages'
 
 const Router: React.FC = () => {
   return (
@@ -17,6 +18,7 @@ const Router: React.FC = () => {
         <Routes>
           <Route path='/' element={<PrivateRoute />}>
             <Route path="/" element={<MakeSurveyList />} />
+            <Route path="/surveys" element={<SurveyResult />} />
           </Route>
           <Route path="/login" element={<MakeLogin />} />
           <Route path="/signup" element={<MakeSignUp />} />

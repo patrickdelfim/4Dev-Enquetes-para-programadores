77cf0bf7ec74560febfb897fd997177235dda56d
Patrick Delfim <patrickdelfim@gmail.com>
2022-05-07 13:50:25 -0300

    test: ensure SurveyResult calls LoadSurveyResult

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,19 +1,28 @@
 import { ApiContext } from '@/presentation/context'
 import { SurveyResult } from '@/presentation/pages'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
-import { mockAccountModel } from '@/domain/test'
+import { LoadSurveyResultSpy, mockAccountModel } from '@/domain/test'
 
-const makeSut = (): void => {
+type SutTypes = {
+  loadSurveyResultSpy: LoadSurveyResultSpy
+}
+
+const makeSut = (): SutTypes => {
   const history = createMemoryHistory({ initialEntries: ['/'] })
+  const loadSurveyResultSpy = new LoadSurveyResultSpy()
   const setCurrentAccountMock = jest.fn()
   render(<ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => mockAccountModel() }}>
     <Router location={history.location} navigator={history}>
-      <SurveyResult />
+      <SurveyResult loadSurveyResult={loadSurveyResultSpy} />
     </Router>
   </ApiContext.Provider>)
+
+  return {
+    loadSurveyResultSpy
+  }
 }
 describe('SurveyResult Component', () => {
   test('should present correct initial state', async () => {
@@ -22,5 +31,12 @@ describe('SurveyResult Component', () => {
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+    await waitFor(() => surveyResult)
+  })
+
+  test('should call LoadSurveyResult', async () => {
+    const { loadSurveyResultSpy } = makeSut()
+    await waitFor(() => screen.getByTestId('survey-result'))
+    expect(loadSurveyResultSpy.callsCount).toBe(1)
   })
 })

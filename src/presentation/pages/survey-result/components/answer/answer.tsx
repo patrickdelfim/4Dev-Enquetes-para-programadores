e46b31294c1905f549e05e2049c42cac4ec6300c
Patrick Delfim <patrickdelfim@gmail.com>
2022-05-08 18:03:00 -0300

    test: ensure SurveyResult present correct data on success

--- src/presentation/pages/survey-result/components/answer/answer.tsx
+++ src/presentation/pages/survey-result/components/answer/answer.tsx
@@ -16,7 +16,7 @@ const Answer: React.FC<Props> = ({ answer }: Props) => {
   return (
     <li data-testid="answer-wrap" className={[Styles.answerWrap, activeClassName].join(' ')}>
           {answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} /> }
-          <span data-testid="answer" className={Styles.answer}> {answer.answer}</span>
+          <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
           <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
     </li>
   )

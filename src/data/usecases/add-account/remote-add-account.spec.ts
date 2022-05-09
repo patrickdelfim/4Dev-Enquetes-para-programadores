482554d16248a991ca8477fe992bcc82e2d0f322
Patrick Delfim <patrickdelfim@gmail.com>
2022-05-08 21:15:02 -0300

    refactor: remove unnecessary tests

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -20,18 +20,13 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
   }
 }
 describe('RemoteAddAccount', () => {
-  test('should call HttpClient with correct URL and Method', async () => {
+  test('should call HttpClient with correct values', async () => {
     const url = faker.internet.url()
     const { sut, httpClientSpy } = makeSut(url)
-    await sut.add(mockAddAccountParams())
-    expect(httpClientSpy.url).toBe(url)
-    expect(httpClientSpy.method).toBe('post')
-  })
-
-  test('Should call HttpClient with correct body', async () => {
-    const { sut, httpClientSpy } = makeSut()
     const AddAccountParams = mockAddAccountParams()
     await sut.add(AddAccountParams)
+    expect(httpClientSpy.url).toBe(url)
+    expect(httpClientSpy.method).toBe('post')
     expect(httpClientSpy.body).toEqual(AddAccountParams)
   })
 
